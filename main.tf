b76a3a69f33b28a5cd73a391c3cb165af285ddef
luanliduario <luanliduario@gmail.com>
2025-05-25 17:18:56 -0300

    fix

--- main.tf
+++ main.tf
@@ -1,15 +1,49 @@
-data "aws_vpc" "vpc" {
-  cidr_block = "172.31.0.0/16"
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
 }
 
-data "aws_subnets" "subnets"{
-    filter {
-        name = "vpc-id"
-        values = [data.aws_vpc.vpc.id]
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
     }
+  }
 }
 
-data "aws_subnet" "subnet" {
-  for_each = toset(data.aws_subnets.subnets.ids)
-  id       = each.value
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+resource "kubectl_manifest" "configmap" {
+  yaml_body = file("${path.module}/k8s/configmap.yaml")
+}
+
+resource "kubectl_manifest" "secrets" {
+  yaml_body = file("${path.module}/k8s/secrets.yaml")
+}
+
+resource "kubectl_manifest" "deployment" {
+  yaml_body = file("${path.module}/k8s/deployment.yaml")
+}
+
+resource "kubectl_manifest" "service" {
+  yaml_body = file("${path.module}/k8s/service.yaml")
+}
+
+resource "kubectl_manifest" "hpa" {
+  yaml_body = file("${path.module}/k8s/hpa.yaml")
 }
