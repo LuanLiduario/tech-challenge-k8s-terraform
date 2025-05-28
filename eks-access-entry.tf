7c33f8295b5f572ff91cd2cedc2f4dd0b6e14769
luanliduario <luanliduario@gmail.com>
2025-05-28 18:52:15 -0300

    fix

--- eks-access-entry.tf
+++ eks-access-entry.tf
@@ -1,6 +1,6 @@
-resource "aws_eks_access_entry" "eks_access_entry" {
+resource "aws_eks_access_entry" "eks_access_entry_labrole" {
   cluster_name      = aws_eks_cluster.eks-cluster.name
-  principal_arn     = var.principalArn
+  principal_arn     = var.labRole
   kubernetes_groups = ["fiap"]
   type              = "STANDARD"
 }
--- eks-access-policy.tf
+++ eks-access-policy.tf
@@ -1,11 +1,11 @@
-resource "aws_eks_access_policy_association" "eks_access_policy" {
+resource "aws_eks_access_policy_association" "eks_access_policy_labrole" {
   cluster_name  = aws_eks_cluster.eks-cluster.name
   policy_arn    = var.policyArn
-  principal_arn = var.principalArn
+  principal_arn = var.labRole
 
   access_scope {
     type = "cluster"
   }
 
-  depends_on = [aws_eks_access_entry.eks_access_entry]
+  depends_on = [aws_eks_access_entry.eks_access_entry_labrole]
 }
